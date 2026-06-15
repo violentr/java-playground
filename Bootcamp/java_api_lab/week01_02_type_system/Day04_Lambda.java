44244bf3eabb9b2a4d5fd9bb7b694b4fa43f3ce5
Deniss B <den.b@itservicesgroup.co.uk>
2026-06-15 18:44:36 +0100

    Add playing with lambda

---
+++ Bootcamp/java_api_lab/week01_02_type_system/Day04_Lambda.java
@@ -0,0 +1,53 @@
+package java_api_lab.week01_02_type_system;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+class User {
+    private String name;
+    public User(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
+public class Day04_Lambda {
+    public static void main(String[] args) {
+        List<User> people = new ArrayList<>(Arrays.asList(
+                new User("John"), new User("Jane"), new User("Audrey"))
+        );
+        printAll(people);
+
+        System.out.println("Printing all user's names with stream:");
+        people.stream().map(User::getName).forEach(System.out::println);
+
+        System.out.println("Printing all user's names with lambda:");
+        people.forEach(p -> System.out.println("Name: " + p.getName()));      // lambda
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 3, 4, 6, 7, 9));
+        isEven(list);
+
+    }
+    public static void printAll(List<User> people){
+        List <String> names = people.stream()
+                /* lambda expression */
+                .filter(p -> p.getName().startsWith("J"))
+                /* method reference  similar to p -> p.getName() 1st parameter is object*/
+                .map(User::getName)
+                .toList();
+        System.out.println(names);
+    }
+    public static void isEven(ArrayList<Integer> list){
+        /* lambda expression */
+        Predicate<Integer> isEven = i -> i % 2 == 0;
+        for (int number : list){
+            if (isEven.test(number)){
+                System.out.printf("%d is even%n", number);
+            }
+        }
+    }
+
+}
