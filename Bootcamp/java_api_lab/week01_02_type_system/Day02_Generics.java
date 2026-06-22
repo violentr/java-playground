85855d6b66b105793508e866d2582fc806dbbaf7
Deniss B <den.b@itservicesgroup.co.uk>
2026-06-22 10:48:27 +0100

    Add generic static method with <extends Comparable>

--- Bootcamp/java_api_lab/week01_02_type_system/Day02_Generics.java
+++ Bootcamp/java_api_lab/week01_02_type_system/Day02_Generics.java
@@ -2,27 +2,51 @@ package java_api_lab.week01_02_type_system;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-  class Box<T> {
+
+class Box<T> {
     private T value;
+
     public Box(T value) {
         this.value = value;
     }
+
     public T getValue() {
         return value;
     }
+
     public void setValue(T value) {
         this.value = value;
+    }
+
+    public void print(T[] array) {
+        for (T value : array) {
+            System.out.println(value);
+        }
+    }
+
+    public static <E extends Comparable<E>> E max(E a, E b) {
+        return a.compareTo(b) >= 0 ? a : b;
+    }
+    public static <E> Box<E> of(E value) {
+        return new Box<>(value);
     }
 }
 
 public class Day02_Generics {
 
     public static void main(String[] args) {
-        Box <Integer> box = new Box<>(3);
+        Box<Integer> box = new Box<>(3);
         System.out.println(box.getValue());
 
-        Box <Character> box2 = new Box<>('a');
+        Box<Double> newBox = new Box<>(3.0);
+        newBox.print(new Double[]{10.2, 20.3, 30.2});
+
+        Box<Character> box2 = new Box<>('a');
         System.out.println(box2.getValue());
+        System.out.println("MAX: " + Box.max(5, 10));
+
+        Box<String> box3 = Box.of("Hello");
+        System.out.println(box3.getValue());
 
         Box<HashMap<String, Integer>> box4 = new Box<>(new HashMap<String, Integer>());
         /* this is possible because HashMap is a mutable object
